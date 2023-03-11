cb86f75ce6c25145b6c778af8c342331c90d3054
Mihir02324 <mihirraj444@gmail.com>
2023-03-12 01:05:30 +0530

    smc

--- server/database/db.ts
+++ server/database/db.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const USERNAME = process.env.USERNAME;
-const PASSWORD = process.env.PASSWORD;
 
 export const Connection = () => {
-  const mongoDB_url = `mongodb://${USERNAME}:${PASSWORD}@ac-7lkp6wr-shard-00-00.plbgwb7.mongodb.net:27017,ac-7lkp6wr-shard-00-01.plbgwb7.mongodb.net:27017,ac-7lkp6wr-shard-00-02.plbgwb7.mongodb.net:27017/?ssl=true&replicaSet=atlas-j9cjwe-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  const mongoDB_url = process.env.MONGODB_URL
+  if (!mongoDB_url) {
+    throw new Error("MongoDB URL is not defined in the environment variables");
+  }
   mongoose.set(`strictQuery`, false);
   mongoose.connect(mongoDB_url);
   mongoose.connection.on("connected", () => {
